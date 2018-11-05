8e511beb59be4cc20ce41715e2d93a4df18a1f45
Timotei Dumitru <dumitru.timy@gmail.com>
2018-11-05 17:24:39 +0000

    implement the structure and the style and functional state of FormUserDetails and FormPersonalDetails components

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import FormUserDetails from './FormUserDetails'
+import FormPersonalDetails from './FormPersonalDetails'
 
 class UserForm extends Component {
   state = {
@@ -11,12 +13,58 @@ class UserForm extends Component {
     bio: ''
   }
 
+  // Proced to next step
+  nextStep = () => {
+    const {step} = this.state;
+    this.setState({
+      step: step + 1
+    })
+  }
+
+  // Go back to previous step
+  prevStep = () => {
+    const {step} = this.state;
+    this.setState({
+      step: step - 1
+    })
+  }
+
+  // Handle fields change
+  handleChange = input => e => {
+    this.setState({
+      [input]: e.target.value
+    })
+  }
+
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    const { step } = this.state;
+    const { firstName, lastName, email, occupation, city, bio } = this.state;
+    const values = { firstName, lastName, email, occupation, city, bio }
+    
+    
+    switch(step) {
+      case 1: 
+        return (
+          <FormUserDetails 
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        )
+      case 2:
+        return (
+          <FormPersonalDetails 
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 3:
+        return <h1>Confirm</h1>;
+      case 4:
+        return <h1>Success</h1>;
+    }
   }
 }
 
