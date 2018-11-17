4685aedac40639464dc9798a08e2642dd966da0f
Timotei Dumitru <dumitru.timy@gmail.com>
2018-11-17 14:34:07 +0000

    Updates

---
+++ precache-manifest.d432f51903ccbd1acb2dd1b9afbb76a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa4d2c916052210f6e5d",
+    "url": "/react-cool-apps/static/js/runtime~main.fa4d2c91.js"
+  },
+  {
+    "revision": "6af8b73a147f90975b65",
+    "url": "/react-cool-apps/static/js/main.6af8b73a.chunk.js"
+  },
+  {
+    "revision": "0fb86361b533cbef4cdc",
+    "url": "/react-cool-apps/static/js/1.0fb86361.chunk.js"
+  },
+  {
+    "revision": "6af8b73a147f90975b65",
+    "url": "/react-cool-apps/static/css/main.858f9613.chunk.css"
+  },
+  {
+    "revision": "b30f4820b80ca7ef7bea2e6a3f1a5785",
+    "url": "/react-cool-apps/index.html"
+  }
+];
